1c4807c1b652aaf80be720b389680d2a9512e94b
Jason Zheng <jzhen127@binghamton.edu>
2021-11-01 18:05:27 -0400

    Milestone 2

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #import your controller
+import pygame
 
 def main():
+    pygame.init()
+    team = {"lead": "Lukas", "backend": "Avery", "frontend": "Jason"}
+    print("Software Lead is:", team["lead"])
+    print("Backend is:", team["backend"])
+    print("Frontend is:" , team["frontend"])
+
     #Create an instance on your controller object
     #Call your mainloop
     
