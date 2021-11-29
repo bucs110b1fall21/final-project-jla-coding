4de726e563e00759f3fa1ea8d22e9cc9858dea65
Avery vonRoeschlaub <avonroe1@binghamton.edu>
2021-11-29 15:50:35 -0500

    v0.0.1 (see changelog)

--- src/economy.py
+++ src/economy.py
@@ -10,7 +10,12 @@ class Economy:
     
     
     def change_rates(self):
-        self.rate = 100 #TODO: add formula
+        '''
+            Updates the rates for cash and coin
+            args: self
+            returns: None
+        '''
+        self.rate = 100 #TODO: add formula and add rate changing to game loop (!!should not run every frame!!)
         self.history.append(rate): #update history
         if len(self.history) > 20:
             self.history.pop(0)
--- src/interactable.py
+++ src/interactable.py
@@ -3,38 +3,37 @@ import pygame
 class Interactable(pygame.sprite.Sprite):
     
     
-    ID_dictionary = {"computer": 0}    
-    
-    
-    def __init__(self, ID, img_path, x, y):
+    def __init__(self, ID, img_path, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(img_path).convert_alpha()
+        if width > 0 and height > 0:
+            self.image = pygame.transform.scale( pygame.image.load(img_path).convert_alpha() , (width, height) ) #creates scaled version of image input with help from https://stackoverflow.com/questions/21082145/pygame-scaling-a-sprite
+        else:
+            self.image = pygame.image.load(img_path).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.ID = ID
     
     
-    def attempt_interact(self, player): #this is a mess, I hope it doesn't break
+    def attempt_interact(self, player):
+        '''
+            tests to see if interaction is possible between self and player, and returns the interactable ID if it is
+            args: self, Player
+            returns: String or None
+        '''
         pos = player.rect.center
         #create line to test for interactions in
         if player.direction == 'U':
-            line = (pos[0], pos[1], pos[0], pos[1] - player.reach)
+            line = (pos[0], pos[1] - player.reach, pos[0], pos[1])
         elif player.direction == 'L':
-            line = (pos[0], pos[1], pos[0] - player.reach, pos[1])
+            line = (pos[0] - player.reach, pos[1], pos[0], pos[1],)
         elif player.direction == 'R':
-            line = (pos[0], pos[1], pos[0], pos[1] + player.reach))
-        else:
             line = (pos[0], pos[1], pos[0] + player.reach, pos[1])
+        else:
+            line = (pos[0], pos[1], pos[0], pos[1] + player.reach)
         #test if ranges overlap: x1 <= y2 && y1 <= x2 for range(x1,y1) and range(x2,y2)
         #formula from https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-two-integer-ranges-for-overlap
-        if (rect.left <= line[2] and rect.right <= line[0]) and (rect.top <= line[3] and rect.bottom <= line[1]):
-            trigger(self)
-    
-    
-    def trigger(self): #will cause whatever the interactable does to happen
-        if self.ID == 'computer': #not sure what this will do exactly
-            pass
-        pass
+        if (self.rect.left <= line[2] and self.rect.right >= line[0]) and (self.rect.top <= line[3] and self.rect.bottom >= line[1]):
+            return self.ID
     
         
